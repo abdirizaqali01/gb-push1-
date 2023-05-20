5551b0981d79a7be46047e3c3d3ecfcd094d663f
Abdirizaq Ali <abdirizaqbusiness@gmail.com>
2023-05-20 17:49:24 +0300

    second

--- growbotai2/settings.py
+++ growbotai2/settings.py
@@ -121,9 +121,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static')
-# ]
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR,  'templates'),
+    # Add to this list all the locations containing your static files 
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
